f0e2ea1b72f28dc7702e948e91162d105659b697
Birkir Rafn Gudjonsson <birkir.gudjonsson@gmail.com>
2013-12-03 14:47:46 +0100

    Fixed ambiguos column name.

--- classes/Model/Prime/Module/Fieldset/Item.php
+++ classes/Model/Prime/Module/Fieldset/Item.php
@@ -12,19 +12,27 @@ class Model_Prime_Module_Fieldset_Item extends Model_Prime {
 	/**
 	 * @var boolean Add sortable with specific keys
 	 */
-	protected $_sortable = ['prime_module_fieldset_id'];
+	protected $_sortable = array(
+		'prime_module_fieldset_id'
+	);
 
 	/**
 	 * Belongs to relationships
 	 * @var array
 	 */
-	protected $_belongs_to = [
-		'fieldset' => [
+	protected $_belongs_to = array(
+		'fieldset' => array(
 			'model'       => 'Prime_Module_Fieldset',
 			'foreign_key' => 'prime_module_fieldset_id'
-		]
-	];
+		)
+	);
 
+	/**
+	 * Overload model getter for extraction of data JSON field.
+	 *
+	 * @param  string $column
+	 * @return string
+	 */
 	public function get($column)
 	{
 		if ( isset($this->_object['data']) AND is_array($this->_object['data']) AND array_key_exists($column, Arr::get($this->_object, 'data', [])))
@@ -35,21 +43,33 @@ class Model_Prime_Module_Fieldset_Item extends Model_Prime {
 		return parent::get($column);
 	}
 
+	/**
+	 * Convert json data to columns in SQL. For performance we only convert fields needed.
+	 *
+	 * @param  array $columns Columns wanted
+	 * @return ORM
+	 */
 	public function data_to_columns($columns = [])
 	{
 		foreach ($columns as $column)
 		{
 			$this->select([DB::expr(
-				  'SUBSTRING(REPLACE(REPLACE(REPLACE(data,\'{\',\'\'),\'}\',\',\'),\'"\',\'\'),'
-				. 'LOCATE(CONCAT(:column,\':\'),REPLACE(REPLACE(REPLACE(data,\'{\',\'\'),\'}\',\',\'),\'"\',\'\')) + CHAR_LENGTH(CONCAT(:column,\':\')),'
-        		. 'LOCATE(\',\',SUBSTRING(REPLACE(REPLACE(REPLACE(data,\'{\',\'\'),\'}\',\',\'),\'"\',\'\'),'
-                . 'LOCATE(CONCAT(:column,\':\'),REPLACE(REPLACE(REPLACE(data,\'{\',\'\'),\'}\',\',\'),\'"\',\'\')) + CHAR_LENGTH(CONCAT(:column,\':\')))) - 1)'
+				  'SUBSTRING(REPLACE(REPLACE(REPLACE(`'.$this->_object_name.'`.`data`,\'{\',\'\'),\'}\',\',\'),\'"\',\'\'),'
+				. 'LOCATE(CONCAT(:column,\':\'),REPLACE(REPLACE(REPLACE(`'.$this->_object_name.'`.`data`,\'{\',\'\'),\'}\',\',\'),\'"\',\'\')) + CHAR_LENGTH(CONCAT(:column,\':\')),'
+        		. 'LOCATE(\',\',SUBSTRING(REPLACE(REPLACE(REPLACE(`'.$this->_object_name.'`.`data`,\'{\',\'\'),\'}\',\',\'),\'"\',\'\'),'
+                . 'LOCATE(CONCAT(:column,\':\'),REPLACE(REPLACE(REPLACE(`'.$this->_object_name.'`.`data`,\'{\',\'\'),\'}\',\',\'),\'"\',\'\')) + CHAR_LENGTH(CONCAT(:column,\':\')))) - 1)'
 			, [':column' => $column]), 'data_'.$column]);
 		}
 
 		return $this;
 	}
 
+	/**
+	 * Overload load values from database method for extracting JSON data field.
+	 *
+	 * @param  array $values Values to parse
+	 * @return ORM
+	 */
 	protected function _load_values(array $values)
 	{
 		// load values defaults
@@ -68,5 +88,4 @@ class Model_Prime_Module_Fieldset_Item extends Model_Prime {
 		// return self
 		return $this;
 	}
-
-} // End Prime Module Fieldset Item
+}
