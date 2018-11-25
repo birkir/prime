72ee44ead69bd4fd0d39d37dd97ffdf05468a39f
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-11-25 00:31:52 -0500

    Fix base field

--- packages/prime-field/src/index.ts
+++ packages/prime-field/src/index.ts
@@ -1,20 +1,22 @@
 export default class PrimeField {
 
   // Internal Hooks
-  internalExpressHook({ app }) {}
-  internalApolloHook({ queries, mutations }) {}
+
+  // internalExpressHook({ app }) {}
+  // internalApolloHook({ queries, mutations }) {}
 
   // External Lifecycle hooks
-  findAllHook({ findOptions }) {}
-  findHook({ findOptions }) {}
-  createHook() {}
-  updateHook() {}
-  deleteHook() {}
-  apolloHook() {}
+
+  // findAllHook({ findOptions }) {}
+  // findHook({ findOptions }) {}
+  // createHook() {}
+  // updateHook() {}
+  // deleteHook() {}
+  // apolloHook() {}
 
   // GraphQL types
-  GraphQL() {}
-  GraphQLInput() {}
+  GraphQL({ field, queries, contentTypes, resolveFieldType }) {}
+  GraphQLInput({ field, queries, contentType, contentTypes, isUpdate, resolveFieldType }) {}
   GraphQLWhere() {}
 }
 
