204776ae629c8ad5187348d85e31495c3ae11905
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-12-28 19:57:06 -0500

    More debugging info

--- packages/prime-core/src/sequelize.ts
+++ packages/prime-core/src/sequelize.ts
@@ -7,18 +7,24 @@ if (!DATABASE_URL || DATABASE_URL === '') {
   throw new Error('DATABASE_URL not set');
 }
 
+const ssl = String(DATABASE_URL).indexOf('ssl=true') >= 0 || (HEROKU_APP_NAME && String(DATABASE_URL).indexOf('amazonaws.com') >= 0);
+
 if (HEROKU_APP_NAME) {
   console.log('HEROKU_APP_NAME =', HEROKU_APP_NAME);
   console.log('DATABASE_URL =', DATABASE_URL);
+  console.log('SSL = ', String(ssl));
 }
 
-export const sequelize = new Sequelize({
+const options = {
   dialect: 'postgres',
   modelPaths: [`${__dirname}/models`],
   operatorsAliases: Op as any,
   logging: false,
   url: DATABASE_URL,
+  ssl,
   dialectOptions: {
-    ssl: String(DATABASE_URL).indexOf('ssl=true') >= 0 || (HEROKU_APP_NAME && String(DATABASE_URL).indexOf('amazonaws.com') >= 0),
+    ssl,
   },
-});
+};
+
+export const sequelize = new Sequelize(options);
