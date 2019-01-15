c69240e089730bb1ddefd95946a713f83b2b3297
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-01-15 15:10:03 -0500

    Update script works now

--- packages/prime-core/src/utils/updateNpmPackages.ts
+++ packages/prime-core/src/utils/updateNpmPackages.ts
@@ -1,23 +1,26 @@
 import { exec } from 'child_process';
-import whichpm from 'which-pm';
+import * as whichpm from 'which-pm';
 
 export const updateNpmPackages = async (packages: string[]) => {
   const pm = await whichpm(process.cwd());
 
   const args = [
-    pm,
+    pm.name,
     pm.name === 'yarn' ? 'add' : 'install',
     ...packages,
   ];
 
   return new Promise((resolve, reject) => {
-    exec(args.join(' '), (err,  stdout, stderr) => {
+    exec(args.join(' '), (err, res1) => {
       if (err) {
         reject(err)
       }
-      console.log(stdout);
-      console.log(stderr);
-      resolve();
+      exec('"$(npm bin)/primecms" db:init', (err, res2) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(Boolean());
+      });
     })
   });
 }
